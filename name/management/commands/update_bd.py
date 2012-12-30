fd52fa3cee92911937309bd3446ec94f9138dc49
l_rahalevich <l_rahalevich@BY1-LL-011.(none)>
2012-12-30 16:49:00 +0300

    update DB

--- name/management/commands/update_bd.py
+++ name/management/commands/update_bd.py
@@ -18,17 +18,39 @@ URL = 'http://vseimena.com'
 
 def _parse_names(soup, gender):
     for name in soup.findAll('div', attrs={'class': 'one_name'}):
-        name_link=dict(name.find('a').attrs)['href']
-        name = name.find('a').text
+        try:
+            name_link=dict(name.find('a').attrs)['href']
+            name = name.find('a').text
+        except AttributeError:
+            name_link=''
+            name = name.text
         print 'name: ', name
         n, created = Name.objects.get_or_create(name=name, gender=gender)
         n.link = URL + name_link
         n.save()
         # get description
-        if not n.description:
+        if not n.description and name_link:
             soup = BeautifulSoup(''.join(urllib.urlopen(n.link).readlines()))
             n.description = soup.find('div', attrs={'class': 'pages_body_text'}).text
             n.save()
+
+def _get_urls(url, urls):
+    result = urls
+    recheck = False
+    soup = BeautifulSoup(''.join(urllib.urlopen(url).readlines()))
+    pages = soup.find('div', attrs={'class': 'pagination_top'}).findChildren('a')
+    for page in pages:
+        url = URL + dict(page.attrs)['href']
+        result.append(url)
+    for url in result:
+        soup = BeautifulSoup(''.join(urllib.urlopen(url).readlines()))
+        new_pages = soup.find('div', attrs={'class': 'pagination_top'}).findChildren('a')
+        for p in new_pages:
+            new_url = URL + dict(p.attrs)['href']
+            if new_url not in result:
+                result.append(new_url)
+                recheck = True
+    return result, recheck
         
 
 class Command(BaseCommand):
@@ -38,17 +60,20 @@ class Command(BaseCommand):
         gender = 1
         soup = BeautifulSoup(''.join(urllib.urlopen(URL).readlines()))
         letter_links = soup.find('div', attrs={'class': 'female_letters'}).findChildren('a')
-        for link in letter_links:
+        for link in letter_links:  #13, 0
             print 'Process link: ', link
             link = dict(link.attrs)['href']
             url = URL + link
-            soup = BeautifulSoup(''.join(urllib.urlopen(url).readlines()))
-            pages = soup.find('div', attrs={'class': 'pagination_top'}).findChildren('a')
-            #parse current link
-            _parse_names(soup, gender)
+            urls = []
+            urls, recheck = _get_urls(url, urls)
+            print urls, recheck
+            while recheck:
+                urls, recheck = _get_urls(url, urls)
+            urls.append(url)
+            urls = list(set(urls))
+            print urls
 
             # parse other pages of letter
-            for page in pages:
-                url = URL + dict(page.attrs)['href']
+            for url in urls:
                 soup = BeautifulSoup(''.join(urllib.urlopen(url).readlines()))
                 _parse_names(soup, gender)
